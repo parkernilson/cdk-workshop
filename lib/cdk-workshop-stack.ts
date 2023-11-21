b162dc33d2db9bdfbab350d1a68b38f09e05c02a
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-21 12:00:09 -0700

    initial commit

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,32 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const vpc = new ec2.Vpc(this, "myVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      availabilityZones: ["us-west-1a"],
+      subnetConfiguration: [
+        {
+          name: "application",
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/get-set-up-for-amazon-ec2.html
+    // How to create a keypair for my instance?
+    // do I need a security group? 
+
+    const instance = new ec2.Instance(this, "myEc2", {
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+    })
   }
 }
